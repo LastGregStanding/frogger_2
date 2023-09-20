33ac7638822343b8eef44b83815b1b56e530ea5c
LastGregStanding <125837709+LastGregStanding@users.noreply.github.com>
2023-09-19 19:36:17 -0700

    added the game design

--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 "use strict";
+
+const gameboard = document.querySelector(".game");
+const cells = [];
+let width = 15;
+const layout = [
+  3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 3, 4, 3, 4, 3, 4, 3, 4, 3,
+  4, 3, 4, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+  2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+  2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+  2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0,
+];
+
+function buildGame() {
+  for (let i = 0; i < layout.length; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    switch (layout[i]) {
+      case 0:
+        cell.classList.add("land");
+        break;
+      case 1:
+        cell.classList.add("road");
+        break;
+      case 2:
+        cell.classList.add("water");
+        break;
+      case 3:
+        cell.classList.add("grass");
+        break;
+      case 4:
+        cell.classList.add("water");
+        cell.classList.add("home");
+        break;
+    }
+    cells.push(cell);
+    gameboard.appendChild(cell);
+  }
+}
+
+buildGame();
